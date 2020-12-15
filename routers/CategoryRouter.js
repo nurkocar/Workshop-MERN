ff444e968913b9ff076fdf62cc91fca161c78ed1
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-12-15 20:22:58 +0000

    Added validations

--- routers/CategoryRouter.js
+++ routers/CategoryRouter.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const CategoryController = require('../controllers/CategoryController');
+const validations = require('../middleware/validationMiddleware');
 
-
-router.post('/addCategory', CategoryController.addCategory);
-
+router.post('/addCategory', validations.categoryValidation, CategoryController.addCategory);
 router.get('/getCategory/:id', CategoryController.getCategory);
-
 router.post('/updateCategory', CategoryController.updateCategory);
-
 router.get('/deleteCategory/:id', CategoryController.deleteCategory);
-
 router.get('/', CategoryController.getCategories);
 
 module.exports = router;
